68e9ee165d8855da0bcadbd3612a4f5bfa417fe7
TheMasterOfIron <league@WTS19.attlocal.net>
2017-08-09 11:54:14 -0700

    day3

--- src/day1/robot/TriangleShell.java
+++ src/day1/robot/TriangleShell.java
@@ -8,22 +8,24 @@ public class TriangleShell {
 Robot buster = new Robot();
 	
 	void go() {
-		drawTriangle(100); // 3. delete this line (used only for testing)
+		 // 3. delete this line (used only for testing)
 
 		// 6. Make the robot go as fast as possible
-
+buster.setSpeed(10);
 		// 4. make a variable to hold the length of the triangle and set it to 50
-
+int l = 50;
 		// 7. Do the following (up to step 10) 60 times
+for (int i = 0; i < 60; i++) {
+	
 
 			// 9. Change the color of the pen to a random color
-	
+	buster.setRandomPenColor();
 			// 8. Increase the length of the side by 10 pixels
-	
+	l = l + 10
 			// 5. call your drawTriangle() method using your length variable
-	
+	drawTriangle(l);
 			// 10. Turn the tortoise 6 degrees to the right
-
+}
 	}
 
 	/* 2. fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
---
+++ src/day3/HelloWorld.java
@@ -0,0 +1,21 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+
+
+
+
+public static void main(String[] args) {
+
+
+System.out.println("Buster is the best");
+String name = JOptionPane.showInputDialog("show your name");
+JOptionPane.showMessageDialog(null, "Hi "+ name);
+}
+
+
+
+
+}
